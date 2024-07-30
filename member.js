b3c4932ab65002faa94e427a764108ef7d4afc91
TaliaWang <talia6075881@gmail.com>
2024-07-30 20:38:06 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMembers() {
+    const members = [
+        {
+            name: 'John Doe',
+            skills: ['HTML', 'CSS', 'JavaScript']
+        },
+        {
+            name: 'Jane Doe',
+            skills: ['JavaScript', 'UX']
+        },
+        {
+            name: 'Jim Doe',
+            skills: ['HTML', 'Node.js']
+        },
+    ];
+
+    return members;
+}
